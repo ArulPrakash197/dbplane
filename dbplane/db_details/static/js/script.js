c1b8f007bcf3a035ccd7964530cf5715bac3cffd
ArulPrakash197 <arulprakash197@gmail.com>
2026-01-28 22:27:45 +0530

    made changes

--- dbplane/db_details/static/js/script.js
+++ dbplane/db_details/static/js/script.js
@@ -17,3 +17,20 @@ else{
 alert("Cancelled");
 }
 }
+// For Pass Eye icons
+const iconOpen = `<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle>`;
+const iconClosed = `<path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.06M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line>`;
+
+window.togglePassword = function() {
+  const input = document.getElementById("myInput");
+  const svg = document.getElementById("eye-svg");
+
+  if (input.type === "password") {
+    input.type = "text";
+    svg.innerHTML = iconClosed; // Swaps to eye-with-slash
+  } else {
+    input.type = "password";
+    svg.innerHTML = iconOpen;   // Swaps back to normal eye
+  }
+};
+//Password Eye Icon end here
